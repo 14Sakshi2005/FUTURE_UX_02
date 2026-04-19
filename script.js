e03bbbf4ffc08fd48503a4394702c411a5979660
Sakshi Satish Yadav <sakshii.yadav14@gmail.com>
2026-04-19 17:17:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function goTo(screenId) {
+  const screens = document.querySelectorAll(".screen");
+
+  screens.forEach(screen => {
+    screen.classList.remove("active");
+  });
+
+  document.getElementById(screenId).classList.add("active");
+}
